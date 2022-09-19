a71fcec64c6d94ad49532f29d4f081dfe8a78666
TimRathert <rathertt@gmail.com>
2022-09-19 12:50:12 -0400

    working on posting to api

--- models/anonimusic_schema.js
+++ models/anonimusic_schema.js
@@ -9,7 +9,7 @@ const commentSchema = new mongoose.Schema ({
 const PostSchema = new mongoose.Schema ({
     user:{type: String, required:true},
     title:{type: String, required:true},
-    post:{type: String, required:true},
+    description:{type: String},
     file:{type: String, required: true},
     tags: [String],
     comments: [commentSchema],
